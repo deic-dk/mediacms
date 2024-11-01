4562be65a2c35e003e131451956fda94644ab5b9
Frederik Orellana <frederik@orellana.dk>
2024-11-01 21:30:16 +0100

    Run chunk combining, final file saving and cleanup in a separate thread.

--- uploader/views.py
+++ uploader/views.py
@@ -9,13 +9,18 @@ from django.http import JsonResponse
 from django.views import generic
 
 from cms.permissions import user_allowed_to_upload
-from files.helpers import rm_file
+from files.helpers import rm_file, run_command
 from files.models import Media
 
 from .fineuploader import ChunkedFineUploader
 from .forms import FineUploaderUploadForm, FineUploaderUploadSuccessForm
 
 import traceback
+from datetime import datetime
+
+from io import StringIO
+
+import asyncio
 
 class FineUploaderView(generic.FormView):
     http_method_names = ("post",)
@@ -49,19 +54,34 @@ class FineUploaderView(generic.FormView):
             raise PermissionDenied  # HTTP 403
         return super(FineUploaderView, self).dispatch(request, *args, **kwargs)
 
+    def async_combine_save(self):
+        print("async_combine_save running")
+        self.upload.combine_chunks()
+        media_file = os.path.join(settings.MEDIA_ROOT, self.upload.real_path)
+        with open(media_file, "rb") as f:
+            myfile = File(f)
+            new = Media.objects.create(media_file=myfile, user=self.request.user)
+        print("Removing "+media_file+" "+str(datetime.now()))
+        rm_file(media_file)
+        print("Removing: "+os.path.join(settings.MEDIA_ROOT, self.upload.file_path)+" "+str(datetime.now()))
+        #shutil.rmtree(os.path.join(settings.MEDIA_ROOT, self.upload.file_path))
+        #shutil.rmtree doesn't like nfs
+        cmd = ['rm', '-rf', os.path.join(settings.MEDIA_ROOT, self.upload.file_path)]
+        ret = run_command(cmd)
+
     def form_valid(self, form):
+        os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
         self.upload = ChunkedFineUploader(form.cleaned_data, self.concurrent)
         if self.upload.concurrent and self.chunks_done:
-            try:
-                print("Calling combine_chunks()...")
-                self.upload.combine_chunks()
-            except FileNotFoundError:
-                tb = traceback.format_exc()
-                data = {"success": False, "error": "Error with File Uploading"}
-                print(tb)
-                for line in traceback.format_stack():
-                    print(line.strip())
-                return self.make_response(data, status=400)
+            #self.upload.combine_chunks()
+            #self.upload.real_path = self.upload.storage.save(self.upload._full_file_path, StringIO())
+            self.upload.real_path = self.upload._full_file_path
+            #asyncio.run(self.async_form_valid())
+            import threading
+            t = threading.Thread(target=self.async_combine_save, args=(), name="async_combine_save")
+            t.start()
+            print("returning "+self.upload.real_path)
+            return self.make_response({"success": True, "media_url": "/user/"+self.request.user.username})
         elif self.upload.total_parts == 1:
             self.upload.save()
         else:
@@ -72,10 +92,13 @@ class FineUploaderView(generic.FormView):
         with open(media_file, "rb") as f:
             myfile = File(f)
             new = Media.objects.create(media_file=myfile, user=self.request.user)
-        print("Removing "+media_file)
+        print("Removing "+media_file+" "+str(datetime.now()))
         rm_file(media_file)
-        print("Removing: "+os.path.join(settings.MEDIA_ROOT, self.upload.file_path))
-        shutil.rmtree(os.path.join(settings.MEDIA_ROOT, self.upload.file_path))
+        print("Removing: "+os.path.join(settings.MEDIA_ROOT, self.upload.file_path)+" "+str(datetime.now()))
+        #shutil.rmtree(os.path.join(settings.MEDIA_ROOT, self.upload.file_path))
+        # shutil.rmtree doesn't like nfs
+        cmd = ['rm', '-rf', os.path.join(settings.MEDIA_ROOT, self.upload.file_path)]
+        ret = run_command(cmd)
         return self.make_response({"success": True, "media_url": new.get_absolute_url()})
 
     def form_invalid(self, form):
